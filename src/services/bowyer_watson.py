e7eda81c1fdd7fcb196114b353a9f1c61d10ee56
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-03-26 09:45:41 +0200

    Added radius calculation to circumcenter method and made circumcenter and radius attributes of triangle

--- src/services/bowyer_watson.py
+++ src/services/bowyer_watson.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Triangle:
     """
     A class to represent triangles consisting of 3 points
@@ -5,15 +7,17 @@ class Triangle:
 
     def __init__(self, p1, p2, p3):
         """
-        Constructor that initializes the 3 vertices of the triangle
+        Constructor that initializes the 3 vertices of the triangle, the circumcenter, and the radius.
         """
         self.p1 = (p1[0], p1[1])
         self.p2 = (p2[0], p2[1])
         self.p3 = (p3[0], p3[1])
+        self.center = self.circumcenter()[0]
+        self.radius = self.circumcenter()[1]
 
     def circumcenter(self):
         """
-        Calculates the center point of the circumcircle.
+        Calculates the center point of the circumcircle and its radius.
         """
 
         ax = self.p1[0]
@@ -27,7 +31,10 @@ class Triangle:
         ux = ((ax**2 + ay**2) * (by-cy) + (bx**2 + by**2) * (cy-ay) + (cx**2 + cy**2) * (ay-by)) / d
         uy = ((ax**2 + ay**2) * (cx-bx) + (bx**2 + by**2) * (ax-cx) + (cx**2 + cy**2) * (bx-ax)) / d
 
-        return (ux, uy)
+        radius = np.sqrt((ux - ax)**2 + (uy - ay)**2)
+
+        return ((ux, uy), radius)
+
 
 class BowyerWatson:
     """
--- src/tests/test_bowyer_watson.py
+++ src/tests/test_bowyer_watson.py
@@ -12,9 +12,11 @@ class TestTriangle(unittest.TestCase):
         self.triangle = Triangle(self.p1, self.p2, self.p3)
 
     def test_circumcenter(self):
-        u = self.triangle.circumcenter()
+        u = self.triangle.center
+        r = self.triangle.radius
         d1 = sqrt((u[0] - self.p1[0])**2 + (u[1] - self.p1[1])**2)
         d2 = sqrt((u[0] - self.p2[0])**2 + (u[1] - self.p2[1])**2)
         d3 = sqrt((u[0] - self.p3[0])**2 + (u[1] - self.p3[1])**2)
         self.assertAlmostEqual(d1, d2)
         self.assertAlmostEqual(d2, d3)
+        self.assertAlmostEqual(d1, r)
