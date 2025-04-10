1b5167f0ca64c69abe45ec0c25b1be76cca48d1e
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-04-10 09:19:41 +0300

    Fixed tests to include renamed variables

--- src/tests/test_prim.py
+++ src/tests/test_prim.py
@@ -9,12 +9,12 @@ class TestPrim(unittest.TestCase):
         self.dungeon.prim()
 
     def test_mst_has_correct_number_of_edges(self):
-        edges = len(self.dungeon.mst.mst.edges)
-        vertices = len(self.dungeon.mst.mst.vertices)
+        edges = len(self.dungeon.paths.mst.edges)
+        vertices = len(self.dungeon.paths.mst.vertices)
         self.assertEqual(edges, vertices - 1)
 
     @patch("matplotlib.pyplot.show")
     def test_display(self, mock_show):
-        self.dungeon.mst.display()
+        self.dungeon.paths.display()
 
         mock_show.assert_called_once()
