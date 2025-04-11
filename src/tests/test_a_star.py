bd193ff64bc76ccb7cc49e49d3b8f3da43316e54
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-04-11 09:23:41 +0300

    Added first test for astar

--- src/tests/test_a_star.py
+++ src/tests/test_a_star.py
@@ -1,5 +1,6 @@
 import unittest
 from dungeon import Dungeon
+from services.a_star import AStar
 
 class TestAStar(unittest.TestCase):
     def setUp(self):
@@ -7,3 +8,15 @@ class TestAStar(unittest.TestCase):
         self.dungeon.place_rooms()
         self.dungeon.prim()
         self.dungeon.a_star()
+
+    def test_astar_finds_path(self):
+        start_room = self.dungeon.rooms[0]
+        goal_room = self.dungeon.rooms[-1]
+
+        start = (round(start_room.center_x), round(self.dungeon.height - start_room.center_y))
+        goal = (round(goal_room.center_x), round(self.dungeon.height - goal_room.center_y))
+
+        path = AStar(self.dungeon).find_path(start, goal)
+        self.assertEqual(path[-1], start)
+        self.assertEqual(path[0], goal)
+        
